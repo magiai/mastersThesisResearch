5cfd67dc77ace8575bebe674453f8e52dd451b84
Dominika <adamskdominik@gmail.com>
2018-01-17 22:18:42 +0100

    First question with connection to mySQL

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Document</title>
+<!--    <link rel="stylesheet" href="css/style.css">-->
+<!--    <script src="script.js"></script>-->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+
+<?php require_once('form.php') ?>
+
+<?php include('questionsRI.php')?>
+
+
+
+</body>
+</html>
---
+++ questionsRI.php
@@ -0,0 +1,23 @@
+<form class="formRI" action="form.php" method="post">
+
+    <!-- Q1RIv1 - Q1 = question 1, RI = różnice indywidualne, v1 = zdecydowanie nie zgadzam się, v5 = Zdecydowanie zgadzam się -->
+
+    <div class="RIquestionBox">
+        <h4 class="question">1. Prowadzę intensywny tyb życia, obfity w wydarzenia.</h4>
+        <ul class="likertRI">
+            <li class="likertRIElement">Zdecydowanie nie zgadzam się<input id="scaleStart" type="radio" name="Q1RI" value="1" /></li>
+            <li class="likertRIElement">Nie zgadzam się<input type="radio" name="Q1RI" value="2" /></li>
+            <li class="likertRIElement">Nie mam zdania<input type="radio" name="Q1RI" value="3" /></li>
+            <li class="likertRIElement">Zgadzam się<input type="radio" name="Q1RI" value="4" /></li>
+            <li class="likertRIElement">Zdecydowanie zgadzam się<input type="radio" name="Q1RI" value="5"/></li>
+        </ul>
+        <br>
+        <div class="nextQuestionBox">
+            <button class="nextQuestion"><input type="submit" name="submit" value="Save" /></button>
+        </div>
+    </div>
+    <br>
+
+
+
+</form>
