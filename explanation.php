f364e1219e156317d59196b44aa3f6b9768e8613
Dominika <adamskdominik@gmail.com>
2018-01-21 02:43:40 +0100

    Explanation part - beginning

--- explanation.php
+++ explanation.php
@@ -1,7 +1,34 @@
 <div class="mainContainer">
 
 
+<div class = "miniPicsBox">
 
-blebleble
+    <div class = "row">
+        <div class = "rowElement">
+            <div class="smallPic smallPic1"></div>
+            <div class = "author"></div>
+        </div>
+        <div class = "rowElement">
+            <div class="smallPic smallPic2"></div>
+            <div class = "author"></div>
+        </div>
+        <div class = "rowElement">
+            <div class="smallPic smallPic3"></div>
+            <div class = "author"></div>
+        </div>
+        <div class = "rowElement">
+            <div class="smallPic smallPic4"></div>
+            <div class = "author"><p>Barbara Kasten<br>Other Works 2</p></div>
+        </div>
+        <div class = "rowElement">
+            <div class="smallPic smallPic5"></div>
+            <div class = "author"></div>
+        </div>
+    </div>
+
+
+
+
+</div>
 
 </div>
